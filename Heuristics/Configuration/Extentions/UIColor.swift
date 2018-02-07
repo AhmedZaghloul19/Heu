5f1d790e9a8bf83583a53eac22526a29aac36c82
Ahmed Hussien <ahmed.abdulatiif@gmail.com>
2018-02-07 22:28:06 +0200

    Add app configuration

---
+++ Heuristics/Configuration/Extentions/UIColor.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor.swift
+//  Heuristics
+//
+//  Created by Ahmed Hussien on 2/7/18.
+//  Copyright © 2018 Heuristic Gang. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var mainAppColor: UIColor {
+        return UIColor(red: 241/255.0, green: 241/255.0, blue: 241/255.0, alpha: 1.0)
+    }
+    
+    static var fedora: UIColor {
+        return UIColor(red: 130/255.0, green: 115/255.0, blue: 130/255.0, alpha: 1.0)
+    }
+}
